106aacfa2a4dd3240cdbee7a24c22d26588ae23c
Alex-nemec <nemec1305@gmail.com>
2023-05-07 19:37:47 +0600

    OOP. Hello World

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+// Однострочный комментарий. Создаем Класс Main
+public class Main {
+    public static void main(String[] args) {
+
+            /* Здесь можно указать
+        многострочный комментарий
+                 */
+
+        System.out.println("Привет, мир! Я изучаю Java.");
+
+}
+
+}
